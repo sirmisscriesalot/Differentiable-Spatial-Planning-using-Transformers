dd6dd5d9c934f91a91b8ae8de981dce090d7c1a4
JhadotAnimesh <jha.animesh01@gmail.com>
2021-12-09 17:02:36 +0530

    fix positional encodings

--- run.py
+++ run.py
@@ -63,7 +63,7 @@ print(f'Using {device} device')
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model=64, max_len=900):
+    def __init__(self, d_model=900, max_len=64):
         """
         Args
             d_model: Hidden dimensionality of the input.
@@ -72,12 +72,17 @@ class PositionalEncoding(nn.Module):
         super().__init__()
 
         # Create matrix of [SeqLen, HiddenDim] representing the positional encoding for max_len inputs
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        pe = torch.zeros(max_len,d_model)
+        position = torch.arange(0, d_model, dtype=torch.float).unsqueeze(1)
+        print(position.shape)
         div_term = torch.exp(torch.arange(
-            0, d_model, 2).float() * (-math.log(900) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
+            0, max_len, 2).float() * (-math.log(900) / max_len))
+        print(div_term.shape)
+        
+        pe[0::2,:] = torch.transpose(torch.sin(position * div_term),0,1)
+        pe[1::2,:] = torch.transpose(torch.cos(position * div_term),0,1)
+
+        
         pe = pe.unsqueeze(0)
 
         # register_buffer => Tensor which is not a parameter, but should be part of the modules state.
@@ -88,7 +93,19 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + self.pe[:, : x.size(1)]
         return x
+    
+encod_block = PositionalEncoding(d_model=900, max_len=64)
+pe = encod_block.pe.squeeze().T.cpu().numpy()
 
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 3))
+pos = ax.imshow(pe, cmap="RdGy", extent=(1, pe.shape[1] + 1, pe.shape[0] + 1, 1))
+fig.colorbar(pos, ax=ax)
+ax.set_xlabel("Position in sequence")
+ax.set_ylabel("Hidden dimension")
+ax.set_title("Positional encoding over hidden dimensions")
+ax.set_xticks([1] + [i * 10 for i in range(1, 1 + pe.shape[1] // 10)])
+ax.set_yticks([1] + [i * 10 for i in range(1, 1 + pe.shape[0] // 10)])
+plt.show()
 
 class DSPT(nn.Module):
 
@@ -100,10 +117,11 @@ class DSPT(nn.Module):
             nn.ReLU(),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1),
             nn.ReLU(),
-            nn.Flatten()
+            nn.Flatten(start_dim=2),
         )
+
         # # Encoder
-        self.pe = PositionalEncoding(d_model=64, max_len=900)
+        self.pe = PositionalEncoding(d_model=900, max_len=64)
         self.encoder = nn.ModuleList()
         self.num_trans_layers = 5
         for _ in range(self.num_trans_layers):
@@ -116,6 +134,7 @@ class DSPT(nn.Module):
     def forward(self, x):
         x = self.p1(x)
         x = self.pe(x)
+        
         for i in range(self.num_trans_layers):
             x = self.encoder[i](x)
         x = self.decoder(x)
