9275fcf988860f052140c979877d3369b0e16a6c
Himadri Bhakta <himadribhakta@gmail.com>
2021-12-27 11:56:37 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,191 +1,205 @@
-from tqdm import tqdm
-from torch.utils.data import DataLoader
-import torch.nn as nn
-import torch
-import random
+import torch 
 import math
-from torch.utils.data import Dataset, DataLoader
-import matplotlib.pyplot as plt
-from torch import autograd
-import torch.nn.functional as F
-from torch.optim.lr_scheduler import StepLR
+import random
+import argparse
+from torch.types import Device
 import numpy as np
-from google_drive_downloader import GoogleDriveDownloader as gdd
-gdd.download_file_from_google_drive(file_id='1lORbh70-sTXvY48ARifexxLsYepDcHSx',
-                                    dest_path='/data/navdata_input_30.npy',
-                                    unzip=False)
-gdd.download_file_from_google_drive(file_id='10WwaIDmBo2cfdHJz4O0aABN0mIbF8ByH',
-                                    dest_path='/data/navdata_output_30.npy',
-                                    unzip=False)
+import torch.nn as nn 
+import torch.optim as optim
+import torch.nn.functional as F
+from torch import autograd,Tensor
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader, dataloader
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
+# wandb.init(project="test1")
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--sizet",help="size of the train dataset",type=int)
+parser.add_argument("--sizev",help="size of the validation dataset",type=int)
+parser.add_argument("--xfilet",help="name of the x train dataset")
+parser.add_argument("--yfilet",help="name of the y train dataset")
+parser.add_argument("--xfilev",help="name of the x validation dataset")
+parser.add_argument("--yfilev",help="name of the y validation dataset")
+args = parser.parse_args()
+
+sizet = args.sizet
+sizev = args.sizev
+xfilet = args.xfilet + ".npy"
+yfilet = args.yfilet + ".npy"
+xfilev = args.xfilev + ".npy"
+yfilev = args.yfilev + ".npy"
 
 def random_seed(seed_value, use_cuda):
-    np.random.seed(seed_value)
-    torch.manual_seed(seed_value)
+    np.random.seed(seed_value)  
+    torch.manual_seed(seed_value)  
     random.seed(seed_value)
     if use_cuda:
         torch.cuda.manual_seed(seed_value)
-        torch.cuda.manual_seed_all(seed_value)
+        torch.cuda.manual_seed_all(seed_value)  
         torch.backends.cudnn.deterministic = True
         torch.backends.cudnn.benchmark = False
-
-
 random_seed(42, True)
 
-
 class SyntheticNavigationDataset(Dataset):
-    def __init__(self, x_file, y_file, n):
-        self.x_list = []
-        self.y_list = []
-        with open(x_file, 'rb') as fx, open(y_file, 'rb') as fy:
-            for i in range(n):
-                self.x_list.append(np.load(fx))
-                self.y_list.append(np.load(fy))
 
-    def __len__(self):
-        return len(self.x_list)
+  def __init__(self,x_file,y_file,n):
+    self.x_list = []
+    self.y_list = []
+    with open(x_file,'rb') as fx, open(y_file,'rb') as fy:
+      for i in range(n):
+        self.x_list.append(np.load(fx))
+        self.y_list.append(np.load(fy))
+  
+  def __len__(self):
+    return len(self.x_list)
 
-    def __getitem__(self, idx):
-        x_tensor = torch.from_numpy(self.x_list[idx].astype(np.float32))
-        y_tensor = torch.from_numpy(self.y_list[idx].astype(np.float32))
-        sample = {'x': x_tensor, 'y': y_tensor}
+  def __getitem__(self,idx):
+    x_tensor = torch.from_numpy(self.x_list[idx].astype(np.float32))
+    y_tensor = torch.from_numpy(self.y_list[idx].astype(np.float32))
+    sample = {'x':x_tensor,'y':y_tensor}
 
-        return sample
+    return sample   
 
-
-nav_dataset = SyntheticNavigationDataset(
-    x_file='/data/navdata_input_30.npy', y_file='/data/navdata_output_30.npy', n=33000)
-sample = nav_dataset[10]
-print(sample['x'])
+train_dataset = SyntheticNavigationDataset(x_file = xfilet, y_file = yfilet,n = sizet)
+validation_dataset = SyntheticNavigationDataset(x_file = xfilev, y_file = yfilev,n = sizev)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Using {device} device')
 
-
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model=900, max_len=64):
-        """
-        Args
-            d_model: Hidden dimensionality of the input.
-            max_len: Maximum length of a sequence to expect.
-        """
-        super().__init__()
 
-        # Create matrix of [SeqLen, HiddenDim] representing the positional encoding for max_len inputs
-        pe = torch.zeros(max_len,d_model)
-        position = torch.arange(0, d_model, dtype=torch.float).unsqueeze(1)
-        print(position.shape)
-        div_term = torch.exp(torch.arange(
-            0, max_len, 2).float() * (-math.log(900) / max_len))
-        print(div_term.shape)
-        
-        pe[0::2,:] = torch.transpose(torch.sin(position * div_term),0,1)
-        pe[1::2,:] = torch.transpose(torch.cos(position * div_term),0,1)
+  def __init__(self):
+    super().__init__()
 
-        
-        pe = pe.unsqueeze(0)
+  def forward(self,x,d_model=64):
+    max_len = x.size(2)
+    pe = torch.zeros(1,d_model,max_len)
+    position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(0)
+    div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(max_len) / d_model)).unsqueeze(1)
+    pe[0,0::2,:] = torch.sin(torch.matmul(div_term,position))
+    pe[0,1::2,:] = torch.cos(torch.matmul(div_term,position))
 
-        # register_buffer => Tensor which is not a parameter, but should be part of the modules state.
-        # Used for tensors that need to be on the same device as the module.
-        # persistent=False tells PyTorch to not add the buffer to the state dict (e.g. when we save the model)
-        self.register_buffer("pe", pe, persistent=False)
+    self.register_buffer('pe',pe)
+    x = x.to(device) + self.pe[:x.size(0)].to(device)
 
-    def forward(self, x):
-        x = x + self.pe[:, : x.size(1)]
-        return x
+    return x
+
+class CNNEncoding(nn.Module):
+
+  def __init__(self):
+    super().__init__()
+
+    self.conv1 = nn.Conv2d(in_channels=2, out_channels=64, kernel_size=1)
+    self.conv2 = nn.Conv2d(in_channels = 64,out_channels=64, kernel_size=1)
+    self.flatten = nn.Flatten(start_dim=2)
+
+  def init_weights(self) -> None:
+    nn.init.kaiming_uniform_(self.conv1.weight.data,nonlinearity='relu')
+    nn.init.kaiming_uniform_(self.conv2.weight.data,nonlinearity='relu')
+
+  def forward(self,x) -> Tensor:
+    x = F.relu(self.conv1(x))
+    x = self.conv2(x)
+    x = self.flatten(x)
     
-encod_block = PositionalEncoding(d_model=900, max_len=64)
-pe = encod_block.pe.squeeze().T.cpu().numpy()
+    return x
 
-fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(8, 3))
-pos = ax.imshow(pe, cmap="RdGy", extent=(1, pe.shape[1] + 1, pe.shape[0] + 1, 1))
-fig.colorbar(pos, ax=ax)
-ax.set_xlabel("Position in sequence")
-ax.set_ylabel("Hidden dimension")
-ax.set_title("Positional encoding over hidden dimensions")
-ax.set_xticks([1] + [i * 10 for i in range(1, 1 + pe.shape[1] // 10)])
-ax.set_yticks([1] + [i * 10 for i in range(1, 1 + pe.shape[0] // 10)])
-plt.show()
+class TransformerModel(nn.Module):
 
-class DSPT(nn.Module):
+  def __init__(self,d_model=64,nhead=8,d_hid=512,nlayers=5,dropout=0.1):
+    super().__init__()
+    self.model_type = 'Transformer'
+    self.pos_encoder = PositionalEncoding()
+    self.conv_encoder = CNNEncoding()
+    encoder_layers = TransformerEncoderLayer(d_model,nhead,d_hid,dropout,batch_first=True)
+    self.transformer_encoder = TransformerEncoder(encoder_layers,nlayers)
+    self.d_model = d_model
+    self.decoder = nn.Linear(d_model,1)
 
-    def __init__(self):
-        # add args dict
-        super().__init__()
-        self.p1 = nn.Sequential(
-            nn.Conv2d(in_channels=2, out_channels=64, kernel_size=1),
-            nn.ReLU(),
-            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=1),
-            nn.ReLU(),
-            nn.Flatten(start_dim=2),
-        )
+  def init_weights(self) -> None:
+    nn.init.kaiming_uniform_(self.encoder.weight.data,nonlinearity='relu')
+    self.decoder.bias.data.zero_()
+    nn.init.kaiming_uniform_(self.decoder.weight.data,nonlinearity='relu')
 
-        # # Encoder
-        self.pe = PositionalEncoding(d_model=900, max_len=64)
-        self.encoder = nn.ModuleList()
-        self.num_trans_layers = 5
-        for _ in range(self.num_trans_layers):
-            self.encoder.append(nn.TransformerEncoderLayer(d_model=900, nhead=9, dim_feedforward=512, dropout=0,
-                                layer_norm_eps=1e-05, batch_first=False, norm_first=False, device=None, dtype=None))
+  def forward(self, src) -> Tensor:
+    src = self.conv_encoder(src)
+    src = self.pos_encoder(src)
+    src = torch.transpose(src,1,2)
+    output = self.transformer_encoder(src)
+    output = self.decoder(output)
 
-        # Decoder
-        self.decoder = nn.Conv1d(in_channels=64, out_channels=1, kernel_size=1)
+    return output
 
-    def forward(self, x):
-        x = self.p1(x)
-        x = self.pe(x)
-        
-        for i in range(self.num_trans_layers):
-            x = self.encoder[i](x)
-        x = self.decoder(x)
-        return x
+model = TransformerModel().to(device)
+criterion = nn.MSELoss()
+lr = 1.0
+optimizer = torch.optim.SGD(model.parameters(),lr=lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer,1,gamma=0.9)
 
+def show_output():
+  plt.subplot(122)
+  sample = train_dataset[10]
+  sample['x'] = torch.reshape(sample['x'],(1,2,sample['x'].shape[1],sample['x'].shape[2]))
+  output = model(sample['x'].to(device))
+  output = torch.reshape(output,(int(math.sqrt(output.shape[1])),int(math.sqrt(output.shape[1]))))
+  output = output.to('cpu').detach().numpy()
 
-model = DSPT()
-model = model.to(device)
-train_dataloader = DataLoader(nav_dataset, batch_size=20, shuffle=True)
+  plt.imshow(output, cmap='hot', interpolation='nearest')
+  plt.show()
 
+def plot_grad_flow(named_parameters):
+    ave_grads = []
+    layers = []
+    for n, p in named_parameters:
+        if(p.requires_grad) and ("bias" not in n):
+            layers.append(n)
+            ave_grads.append(p.grad.abs().mean())
+    plt.subplot(121)
+    plt.plot(ave_grads, alpha=0.3, color="b")
+    plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+    plt.xlim(xmin=0, xmax=len(ave_grads))
+    plt.xlabel("Layers")
+    plt.ylabel("average gradient")
+    plt.title("Gradient flow")
+    plt.grid(True)
 
-def train_loop(dataloader, model, epochs):
-    model.train()
-    loss_fn = nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1)
-    scheduler = StepLR(optimizer, step_size=1, gamma=0.9)
-    # with autograd.detect_anomaly():
-    for t in tqdm(range(epochs)):
-        for batch, sample in enumerate(dataloader):
-            pred = model(sample['x'].to(device))
-            sample['y'] = sample['y'].to(device)
-            loss = loss_fn(torch.reshape(pred, (-1, 30, 30)), sample['y'])
-            if batch % 100 == 0:
-                print(f"loss: {loss.item()}")
-            optimizer.zero_grad()
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-            optimizer.step()
-            scheduler.step()
+train_dl = DataLoader(train_dataset,batch_size=1,shuffle=True)
+valid_dl = DataLoader(validation_dataset,batch_size=2)
 
+def loss_func(inx,outy):
+  return criterion(torch.reshape(inx,(-1,int(math.sqrt(inx.shape[1])),int(math.sqrt(inx.shape[1])))),outy)
 
-def test_loop(dataloader, model):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    test_loss, correct = 0, 0
-    loss_fn = nn.MSELoss()
-    model.eval()
-    with torch.no_grad():
-        for sample in dataloader:
-            pred = model(sample['x'].to(device))
-            sample['y'] = sample['y'].to(device)
-            test_loss += loss_fn(torch.reshape(pred,
-                                 (-1, 30, 30)), sample['y']).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+def train(model: nn.Module) -> None:
 
-    test_loss /= num_batches
-    correct /= size
-    print(
-        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+  model.train()
 
+  for batch,sample in enumerate(train_dl):
+    pred = model(sample['x'].to(device))
+    out = sample['y'].to(device)
+    loss = loss_func(pred,out)
+    optimizer.zero_grad()
+    loss.backward()
+    torch.nn.utils.clip_grad_norm_(model.parameters(),1.0)
+    optimizer.step()
 
-EPOCHS = 40
-train_loop(train_dataloader, model, epochs=EPOCHS)
+    if batch%100 == 0:
+      loss = loss.item()
+      print(f"loss: {loss:>7f}")
+
+epochs = 40
+
+for t in range(epochs):
+  print(f"Epoch {t+1}\n-------------------------------")
+  train(model)
+  scheduler.step()
+
+  model.eval()
+  with torch.no_grad():
+    validation_loss = sum(loss_func(model(val_sample['x'].to(device)),val_sample['y'].to(device)) for val_sample in valid_dl)
+  print(t , validation_loss/len(valid_dl))
+
 print("Done!")
+
+
