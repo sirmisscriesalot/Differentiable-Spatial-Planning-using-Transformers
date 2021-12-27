b60eedeecc888eab3ed6668a09462ebd892ea4a2
Himadri Bhakta <himadribhakta@gmail.com>
2021-12-27 14:13:12 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,13 +2,13 @@ import torch
 import math
 import random
 import argparse
-from torch.types import Device
+# from torch.types import Device
 import numpy as np
 import torch.nn as nn 
 import torch.optim as optim
 import torch.nn.functional as F
 from torch import autograd,Tensor
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader, dataloader
 from torch.nn import TransformerEncoder, TransformerEncoderLayer
 
@@ -137,33 +137,33 @@ lr = 1.0
 optimizer = torch.optim.SGD(model.parameters(),lr=lr)
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer,1,gamma=0.9)
 
-def show_output():
-  plt.subplot(122)
-  sample = train_dataset[10]
-  sample['x'] = torch.reshape(sample['x'],(1,2,sample['x'].shape[1],sample['x'].shape[2]))
-  output = model(sample['x'].to(device))
-  output = torch.reshape(output,(int(math.sqrt(output.shape[1])),int(math.sqrt(output.shape[1]))))
-  output = output.to('cpu').detach().numpy()
+# def show_output():
+#   plt.subplot(122)
+#   sample = train_dataset[10]
+#   sample['x'] = torch.reshape(sample['x'],(1,2,sample['x'].shape[1],sample['x'].shape[2]))
+#   output = model(sample['x'].to(device))
+#   output = torch.reshape(output,(int(math.sqrt(output.shape[1])),int(math.sqrt(output.shape[1]))))
+#   output = output.to('cpu').detach().numpy()
 
-  plt.imshow(output, cmap='hot', interpolation='nearest')
-  plt.show()
+#   plt.imshow(output, cmap='hot', interpolation='nearest')
+#   plt.show()
 
-def plot_grad_flow(named_parameters):
-    ave_grads = []
-    layers = []
-    for n, p in named_parameters:
-        if(p.requires_grad) and ("bias" not in n):
-            layers.append(n)
-            ave_grads.append(p.grad.abs().mean())
-    plt.subplot(121)
-    plt.plot(ave_grads, alpha=0.3, color="b")
-    plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
-    plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
-    plt.xlim(xmin=0, xmax=len(ave_grads))
-    plt.xlabel("Layers")
-    plt.ylabel("average gradient")
-    plt.title("Gradient flow")
-    plt.grid(True)
+# def plot_grad_flow(named_parameters):
+#     ave_grads = []
+#     layers = []
+#     for n, p in named_parameters:
+#         if(p.requires_grad) and ("bias" not in n):
+#             layers.append(n)
+#             ave_grads.append(p.grad.abs().mean())
+#     plt.subplot(121)
+#     plt.plot(ave_grads, alpha=0.3, color="b")
+#     plt.hlines(0, 0, len(ave_grads)+1, linewidth=1, color="k" )
+#     plt.xticks(range(0,len(ave_grads), 1), layers, rotation="vertical")
+#     plt.xlim(xmin=0, xmax=len(ave_grads))
+#     plt.xlabel("Layers")
+#     plt.ylabel("average gradient")
+#     plt.title("Gradient flow")
+#     plt.grid(True)
 
 train_dl = DataLoader(train_dataset,batch_size=1,shuffle=True)
 valid_dl = DataLoader(validation_dataset,batch_size=2)
